755e6a189ad64a739b1b407e234cdc3253db1762
Braian de Leon <braiandeleon@gmail.com>
2023-03-18 13:26:48 -0600

    ;

--- application.py
+++ application.py
@@ -1,24 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 18 09:55:10 2023
-
-@author: braiandeleon
-"""
 
 from flask import Flask, request
-
+import requests
 
 application = Flask(__name__)
 
 @application.route('/')
 def index():
     url = request.args.get('url')
-    param2 = request.args.get('param2')
+    response = requests.get(url)
     # use the parameters in your code
-    response = request.get(url)
-    # Print the content of the response
-    return 'Hello World!'
+    return response.content
 
 if __name__ == '__main__':
     application.run(debug=True)
+    
